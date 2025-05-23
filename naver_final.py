6a7a9589eb13797c2462870c7e09a66042964d35
Min-Jae717 <dbwoals137@gmail.com>
2025-05-23 11:36:01 +0900

    Update naver_final.py

--- naver_final.py
+++ naver_final.py
@@ -17,8 +17,8 @@ import time
 st.set_page_config(page_title="스마트 쇼핑 파인더", layout="wide")
 
 # 네이버 API 클라이언트 ID와 시크릿
-NAVER_CLIENT_ID = "9XhhxLV1IzDpTZagoBr1"
-NAVER_CLIENT_SECRET = "J14HFxv3B6"
+NAVER_CLIENT_ID = st.secrets["NAVER_CLIENT_ID"]
+NAVER_CLIENT_SECRET = st.secrets["NAVER_CLIENT_SECRET"]
 
 # Streamlit에서 실행 중인지 확인하고 secrets 가져오기
 try:
